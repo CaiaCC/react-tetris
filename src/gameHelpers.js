b68dd2227c17d82445496c3818960478556c8600
Caia Chuang <caiachuang@gmail.com>
2021-02-16 21:27:01 -0800

    Create 7 tetrominos and the function that generates a random tetromino.

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,5 +1,8 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
-// create the stage using a multi-dimension(nested) array contains rows and columns(grid)
-export const creatStage = () => Array.from(Array(STAGE_HEIGHT), () => new Array(STAGE_WIDTH).fill([0, 'clear']))
+// create the stage: a multi-dimension(nested) array that contains rows and columns(grid)
+export const creatStage = () => 
+    Array.from(Array(STAGE_HEIGHT), () => 
+        new Array(STAGE_WIDTH).fill([0, 'clear'])
+    )
