7e9d39fd8a112911ebbc62b31b93d62d14086e2d
Caia Chuang <caiachuang@gmail.com>
2021-02-25 21:21:00 -0800

    Set up states and hooks in Tetris component.

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import { randomTetromino } from "../tetrominos";
 
-export const usePalyer = () => {
+export const usePlayer = () => {
     const [player, setPlayer] = useState({
         pos: { x: 0, y: 0 },
         tetromino: randomTetromino().shape,
