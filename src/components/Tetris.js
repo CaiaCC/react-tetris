0708067a2ac66eb49ced49acc5b61439b6f56305
Caia Chuang <caiachuang@gmail.com>
2021-03-05 21:56:20 -0800

    Collision detection.

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { createStage } from '../gameHelpers';
+import { createStage, checkCollision } from '../gameHelpers';
 
 // Styled Components
 import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
@@ -19,22 +19,37 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false);
     
     const [player, updatePlayerPos, resetPlayer] = usePlayer();
-    const [stage, setStage] = useStage(player);
+    const [stage, setStage] = useStage(player, resetPlayer);
 
     console.log('re-render');
 
     const movePlayer = dir => {
-        updatePlayerPos({ x: dir, y: 0 });
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir, y: 0 });
+        }
     }
 
     const startGame = () => {
+        console.log('test');
         // Reset everything
         setStage(createStage());
         resetPlayer();
+        setGameOver(false);
     }
 
     const drop = () => {
-        updatePlayerPos({ x: 0, y: 1, collided: false });
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false });
+        } else {
+            // game over
+            if (player.pos.y < 1) {
+                console.log("GAME OVER!!");
+                setGameOver(true);
+                setDropTime(null);
+            }
+            updatePlayerPos({ x: 0, y: 0, collided: true });
+        }
+            
     }
 
     const dropPlayer = () => {
@@ -44,37 +59,37 @@ const Tetris = () => {
     const move = ({ keyCode }) => {
         if (!gameOver) {
             if (keyCode === 37) {
-              movePlayer(-1);
+                movePlayer(-1);
             } else if (keyCode === 39) {
-              movePlayer(1);
+                movePlayer(1);
             } else if (keyCode === 40) {
-              dropPlayer();
+                dropPlayer();
             }
         }
     }
 
     return (
-      <StyledTetrisWrapper
-        role="button"
-        tabIndex="0"
-        onKeyDown={(e) => move(e)}
-      >
-        <StyledTetris>
-          <Stage stage={stage} />
-          <aside>
-            {gameOver ? (
-              <Display gameOver={gameOver} text="Game Over" />
-            ) : (
-              <div>
-                <Display text="Score" />
-                <Display text="Rows" />
-                <Display text="Level" />
-              </div>
-            )}
-            <StartButton callback={startGame} />
-          </aside>
-        </StyledTetris>
-      </StyledTetrisWrapper>
+        <StyledTetrisWrapper
+          role="button"
+          tabIndex="0"
+          onKeyDown={(e) => move(e)}
+        >
+            <StyledTetris>
+                <Stage stage={stage} />
+                <aside>
+                    {gameOver ? (
+                        <Display gameOver={gameOver} text="Game Over" />
+                    ) : (
+                        <div>
+                            <Display text="Score" />
+                            <Display text="Rows" />
+                            <Display text="Level" />
+                        </div>
+                    )}
+                    <StartButton callback={startGame} />
+                </aside>
+            </StyledTetris>
+        </StyledTetrisWrapper>
     );
 };
 
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -2,7 +2,29 @@ export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
 // create the stage: a multi-dimension(nested) array that contains rows and columns(grid)
-export const createStage = () => 
-    Array.from(Array(STAGE_HEIGHT), () => 
-        new Array(STAGE_WIDTH).fill([0, 'clear'])
-    )
+export const createStage = () =>
+    Array.from(Array(STAGE_HEIGHT), () =>
+        new Array(STAGE_WIDTH).fill([0, "clear"])
+    );
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+    for (let y = 0; y < player.tetromino.length; y++) {
+        for (let x = 0; x < player.tetromino[0].length; x++) {
+        // 1. check that we're on an actual Tetromino cell
+            if (player.tetromino[y][x] !== 0) {
+                if (
+                // 2. check that our move is inside the stage height (y)
+                // we shouldn't go through the bottom of the stage
+                !stage[y + player.pos.y + moveY] ||
+                // 3. check that our move is inside the stage width (x)
+                !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                // 4. check that the cell we're moving to isn't set to clear
+                stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+                    "clear"
+                ) {
+                    return true;
+                }
+            }
+        }
+    }
+};
